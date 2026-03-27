da2d7a7bff0c408aa9029c401623376cf03662af
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-27 08:45:07 +0200

    feat: add layout, head, and styling for notely app

---
+++ app/head.js
@@ -0,0 +1,20 @@
+export default function Head() {
+  return (
+    <head>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Eczar:wght@400..800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Press+Start+2P&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+  );
+}
